13fdf26c63e97cd11c31988308817cc6665f8678
saberahmadmd <msabair@gmail.com>
2025-02-03 09:49:49 +0000

    added charts

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,5 +1,235 @@
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+
+import LatestMatch from '../LatestMatch'
+import MatchCard from '../MatchCard'
+
+import './index.css'
+
+const teamMatchesApiUrl = 'https://apis.ccbp.in/ipl/'
+
+class TeamMatches extends Component {
+  state = {
+    isLoading: true,
+    teamMatchesData: {},
+    matchStats: {
+      won: 0,
+      lost: 0,
+      drawn: 0,
+    },
+  }
+
+  componentDidMount() {
+    this.getTeamMatches()
+  }
+
+  getFormattedData = data => ({
+    umpires: data.umpires,
+    result: data.result,
+    manOfTheMatch: data.man_of_the_match,
+    id: data.id,
+    date: data.date,
+    venue: data.venue,
+    competingTeam: data.competing_team,
+    competingTeamLogo: data.competing_team_logo,
+    firstInnings: data.first_innings,
+    secondInnings: data.second_innings,
+    matchStatus: data.match_status,
+  })
+
+  getTeamMatches = async () => {
+    const {match: routeMatch} = this.props // Renamed 'match' to 'routeMatch'
+    const {params} = routeMatch
+    const {id} = params
+
+    const response = await fetch(`${teamMatchesApiUrl}${id}`)
+    const fetchedData = await response.json()
+    const formattedData = {
+      teamBannerURL: fetchedData.team_banner_url,
+      latestMatch: this.getFormattedData(fetchedData.latest_match_details),
+      recentMatches: fetchedData.recent_matches.map(eachMatch =>
+        this.getFormattedData(eachMatch),
+      ),
+    }
+
+    // Calculate the match stats (won, lost, drawn)
+    const matchStats = {
+      won: 0,
+      lost: 0,
+      drawn: 0,
+    }
+
+    formattedData.recentMatches.forEach(match => {
+      if (match.matchStatus === 'Won') {
+        matchStats.won += 1
+      } else if (match.matchStatus === 'Lost') {
+        matchStats.lost += 1
+      } else {
+        matchStats.drawn += 1
+      }
+    })
+
+    this.setState({
+      teamMatchesData: formattedData,
+      isLoading: false,
+      matchStats,
+    })
+  }
+
+  renderMatchStatsPieChart = () => {
+    const {matchStats} = this.state
+    if (
+      matchStats.won === 0 &&
+      matchStats.lost === 0 &&
+      matchStats.drawn === 0
+    ) {
+      return null
+    }
+
+    const data = [
+      {name: 'Won', value: matchStats.won},
+      {name: 'Lost', value: matchStats.lost},
+      {name: 'Drawn', value: matchStats.drawn},
+    ]
+
+    const COLORS = ['#00C49F', '#FF8042', '#FFBB28']
+
+    return (
+      <>
+        <h1>Statistics</h1>
+        <ResponsiveContainer width="100%" height={300}>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={120}
+              fill="#8884d8"
+            >
+              {data.map(entry => (
+                <Cell
+                  key={entry.name}
+                  fill={COLORS[data.indexOf(entry) % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </ResponsiveContainer>
+      </>
+    )
+  }
+
+  onClickBackButton = () => {
+    const {history} = this.props
+    history.replace('/')
+  }
+
+  renderRecentMatchesList = () => {
+    const {teamMatchesData} = this.state
+    const {recentMatches} = teamMatchesData
+
+    return (
+      <>
+        <ul className="recent-matches-list">
+          {recentMatches.map(recentMatch => (
+            <MatchCard matchDetails={recentMatch} key={recentMatch.id} />
+          ))}
+        </ul>
+        <button
+          className="back-button"
+          type="button"
+          onClick={this.onClickBackButton}
+        >
+          Back
+        </button>
+      </>
+    )
+  }
+
+  renderTeamMatches = () => {
+    const {teamMatchesData} = this.state
+    const {teamBannerURL, latestMatch} = teamMatchesData
+
+    return (
+      <div className="responsive-container">
+        <img src={teamBannerURL} alt="team banner" className="team-banner" />
+        <LatestMatch latestMatchData={latestMatch} />
+        {this.renderMatchStatsPieChart()}
+        {this.renderRecentMatchesList()}
+      </div>
+    )
+  }
+
+  renderLoader = () => (
+    <div className="loader-container" data-testid="loader">
+      <Loader type="Oval" color="#ffffff" height={50} />
+    </div>
+  )
+
+  getRouteClassName = () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    switch (id) {
+      case 'RCB':
+        return 'rcb'
+      case 'KKR':
+        return 'kkr'
+      case 'KXP':
+        return 'kxp'
+      case 'CSK':
+        return 'csk'
+      case 'RR':
+        return 'rr'
+      case 'MI':
+        return 'mi'
+      case 'SH':
+        return 'srh'
+      case 'DC':
+        return 'dc'
+      default:
+        return ''
+    }
+  }
+
+  render() {
+    const {isLoading} = this.state
+    const className = `team-matches-container ${this.getRouteClassName()}`
+
+    return (
+      <div className={className}>
+        {isLoading ? this.renderLoader() : this.renderTeamMatches()}
+      </div>
+    )
+  }
+}
+
+export default TeamMatches
+
+/*
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
 
 import LatestMatch from '../LatestMatch'
 import MatchCard from '../MatchCard'
@@ -50,16 +280,27 @@ class TeamMatches extends Component {
     this.setState({teamMatchesData: formattedData, isLoading: false})
   }
 
+  onClickBackButton = () => {
+    const {history} = this.props
+    history.replace('/')
+  }
+
   renderRecentMatchesList = () => {
     const {teamMatchesData} = this.state
     const {recentMatches} = teamMatchesData
+    console.log(recentMatches)
 
     return (
-      <ul className="recent-matches-list">
-        {recentMatches.map(recentMatch => (
-          <MatchCard matchDetails={recentMatch} key={recentMatch.id} />
-        ))}
-      </ul>
+      <>
+        <ul className="recent-matches-list">
+          {recentMatches.map(recentMatch => (
+            <MatchCard matchDetails={recentMatch} key={recentMatch.id} />
+          ))}
+        </ul>
+        <button type="button" onClick={this.onClickBackButton}>
+          Back
+        </button>
+      </>
     )
   }
 
@@ -122,3 +363,4 @@ class TeamMatches extends Component {
 }
 
 export default TeamMatches
+*/
