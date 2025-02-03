bf5c4b7d55969e55e476ed1a07ec10a982303eec
saberahmadmd <msabair@gmail.com>
2025-02-03 07:57:05 +0000

    solution

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <h1 className="notfound-heading">Page Not found!!</h1>
+  </div>
+)
+
+export default NotFound
